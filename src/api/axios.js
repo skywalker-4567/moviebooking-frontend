e5101083d2c63b47e4c578eab64731fb0e72102e
skywalker-4567 <utkarshksingh.34567@gmail.com>
2026-03-25 23:43:49 +0530

    fix: update axios base url vercel

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:8080',
+const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
 });
 
 // Automatically attach JWT to every request
